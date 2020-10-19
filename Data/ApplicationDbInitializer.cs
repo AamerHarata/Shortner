98fff8cfb45a8d95270037ba61854fe32305ac9a
Aamer Harata <Aamer.harata@hotmail.com>
2020-10-19 14:07:03 +0200

    Fix database in production

--- Data/ApplicationDbInitializer.cs
+++ Data/ApplicationDbInitializer.cs
@@ -14,6 +14,7 @@ namespace Shortner.Data
 
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
+            context.SaveChanges();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Shortner.Data;
 
 namespace Shortner
 {
@@ -13,7 +15,15 @@ namespace Shortner
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var env = services.GetService<IHostEnvironment>();
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                ApplicationDbInitializer.Initializer(context, env.IsDevelopment());
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
